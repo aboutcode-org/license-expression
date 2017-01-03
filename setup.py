93731eaa3c8b01a9ce7edd30d6fb5059209dce84
Philippe Ombredanne <pombredanne@nexb.com>
2017-01-03 16:40:19 +0100

    Bump version
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ desc = ('license-expression is small utility library to parse, compare, '
 
 setup(
     name='license-expression',
-    version='0.7',
+    version='0.8',
     license='apache-2.0',
     description=desc,
     long_description=desc,
