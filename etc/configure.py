514a33d530c8359cd1882fe850da30d2efc9a5bb
Philippe Ombredanne <pombredanne@nexb.com>
2017-11-21 15:17:53 +0100

    Add missing license notice
    Thanks to @carmenbianca and @jonasob
    
    Reported-by: Jonas Öberg <jonas@fsfe.org>
    Reported-by: Carmen Bianca Bakker <carmenbianca@fsfe.org>
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- etc/configure.py
+++ etc/configure.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
-# Copyright (c) 2015 nexB Inc. http://www.nexb.com/ - All rights reserved.
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and http://aboutcode.org
+#
+# This software is licensed under the Apache License version 2.0.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
 
 """
 This script a configuration helper to select pip requirement files to install
