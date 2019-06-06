c2a051748293a6ff3994ab12e911a5b286e7a19b
Philippe Ombredanne <pombredanne@nexb.com>
2019-06-06 12:18:49 +0200

    Try to make configure.bat working on Azure
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- configure.bat
+++ configure.bat
@@ -1,9 +1,9 @@
 @echo ON
 
-@rem Copyright (c) 2016 nexB Inc. http://www.nexb.com/ - All rights reserved.
+@rem Copyright (c) nexB Inc. http://www.nexb.com/ - All rights reserved.
 
 @rem ################################
-@rem # change these variables to customize this script locally
+@rem # Defaults. change these variables to customize this script locally
 @rem ################################
 @rem # you can define one or more thirdparty dirs, each prefixed with TPP_DIR
 set TPP_DIR_BASE=thirdparty/base
@@ -16,23 +16,22 @@ set DEFAULT_PYTHON=python
 set CONF_DEFAULT="etc/conf/dev"
 @rem #################################
 
-set LE_ROOT_DIR=%~dp0
 
-@rem !!!!!!!!!!! ATTENTION !!!!!
-@rem there is a space at the end of the set CFG_CMD_LINE_ARGS=  line ... 
-@rem NEVER remove this!
-@rem otherwise, this script does not work.  
-set CFG_CMD_LINE_ARGS= 
-@rem Collect/Slurp all command line arguments in a variable
+set CFG_ROOT_DIR=%~dp0
+
+@rem a possible alternative way and simpler way to slurp args
+@rem set CFG_CMD_LINE_ARGS=%*
+
+@rem Collect all command line arguments in a variable
+set CFG_CMD_LINE_ARGS=
 :collectarg
- if ""%1""=="""" (
-    goto continue
- )
- call set CFG_CMD_LINE_ARGS=%CFG_CMD_LINE_ARGS% %1
+ if ""%1""=="""" goto continue
+ set CFG_CMD_LINE_ARGS=%CFG_CMD_LINE_ARGS% %1
  shift
- goto collectarg
+goto collectarg
 
 :continue
+
 
 @rem default to dev configuration when no args are passed
 if "%CFG_CMD_LINE_ARGS%"==" " (
@@ -43,16 +42,18 @@ if "%PYTHON_EXE%"==" " (
     set PYTHON_EXE="%DEFAULT_PYTHON%"
 )
 
+@rem run the configuration script proper
+call "%PYTHON_EXE%" %CFG_ROOT_DIR%\etc\configure.py %CFG_CMD_LINE_ARGS%
 
-:configure
-call python %LE_ROOT_DIR%etc\configure.py %CFG_CMD_LINE_ARGS%
 
-
+@rem Return a proper return code on failure
 if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
-if exist "%LE_ROOT_DIR%bin\activate" (
-    "%LE_ROOT_DIR%bin\activate"
+
+@rem Activate the virtualenv
+if exist "%CFG_ROOT_DIR%bin\activate" (
+    "%CFG_ROOT_DIR%bin\activate"
 )
 goto EOS
 
