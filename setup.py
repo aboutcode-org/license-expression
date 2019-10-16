4d1921957ecc2422a1aed0cc462866b4b7a299e7
Steven Esser <sesser@nexb.com>
2019-10-16 13:47:08 -0400

    Bump to version v1.1
    Signed-off-by: Steven Esser <sesser@nexb.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ desc = ('license-expression is small utility library to parse, compare, '
 
 setup(
     name='license-expression',
-    version='1.0',
+    version='1.1',
     license='apache-2.0',
     description=desc,
     long_description=desc,
