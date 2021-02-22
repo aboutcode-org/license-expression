586bb13452505bea0cd34468af0680f151636f6d
Chin Yeung <tli@nexb.com>
2021-02-22 17:59:02 +0800

    Update src/license_expression/__init__.py
    Co-authored-by: Philippe Ombredanne <pombredanne@nexb.com>
    Signed-off-by: Chin Yeung Li <tli@nexb.com>

--- src/license_expression/__init__.py
+++ src/license_expression/__init__.py
@@ -631,7 +631,11 @@ class Licensing(boolean.BooleanAlgebra):
         else:
             dedup_expression = ' OR '.join(expression_list)
         # Put the parentheses between the expression for grouping purpose.
+<<<<<<< HEAD
         #dedup_expression = '(' + dedup_expression + ')'
+=======
+        dedup_expression = '({})'.format(dedup_expression)
+>>>>>>> Update src/license_expression/__init__.py
         return dedup_expression
 
 def build_symbols_from_unknown_tokens(tokens):
